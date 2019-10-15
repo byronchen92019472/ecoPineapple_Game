05ba296f7baa86f0eec609d5de8329c902c085c5
ChungLingTsao <chaz_tsao@hotmail.com>
2019-10-15 13:10:18 +1300

    Added stories. Need to change elon.

--- Assets/Scripts/AnimatedDialog.cs
+++ Assets/Scripts/AnimatedDialog.cs
@@ -1,16 +1,8 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Audio;
-using UnityStandardAssets.CrossPlatformInput;
 using UnityEngine.SceneManagement;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityStandardAssets.CrossPlatformInput;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Runtime.Serialization;
 using System.IO;
 
 public class AnimatedDialog : MonoBehaviour {
@@ -20,6 +12,8 @@ public class AnimatedDialog : MonoBehaviour {
     public float speed = 0.1f;
     public GameObject rubbishImages;
     public GameObject starportImage;
+    public GameObject ecoProductImage;
+    public GameObject touristImage;
     public string[] readString;
     public int levelNumber;
 
@@ -50,31 +44,36 @@ public class AnimatedDialog : MonoBehaviour {
             };
         }
         if (levelNumber == 2){
-            readString  = new string[10] {
-                "asdadsadHello, my name is Elon and I am an investor for this space tourism company",
-                "I have been tasked to guide you in constructing a suitable spacecraft for space tourism",
-                "Your first test is to build a spacecraft that is able to reach our planetary starport",
-                "Be careful though. You want to avoid the space trash layer orbitting the Earth",
-                "Navigate the rocket using the left and right thrusters to avoid the following rubbish obstacles...",
+            readString = new string[11] {
+                "Good job with successfully docking at the spaceport",
+                "\u0022If you get up in the morning and think the future is going to be better, it is a bright day\u0022",
+                "Today will be a bright day for you as you trial our future solution",
+                "For your next mission, you will deliver rubbish to the spaceport of Mercury",
+                "We need to test if it is viable to throw our rubbish into the Sun",
+                "Just be careful to still avoid garbage and any asteroids along the way",
+                "Also, please collect any \u0022rubbish\u0022 that seems reusable",
+                "These are deemed eco-products and we will provide bonus money for each one you collect",
+                "These eco-products will look like this...",
                 "",
-                "Once you are close enough, the starport's tractor beam will guide you in for docking.\n\nThe starport looks like this...",
-                "",
-                "\u0022When something is important enough, you do it even if the odds are not in your favor\u0022",
-                "Good Luck!"
+                "It is going to be fun throwing our problems into the fiery inferno. Let's go!"
             };
         }
         if (levelNumber == 3){
-            readString  = new string[10] {
-                "Hello, my name is Elon and I am an investor for this space tourism company",
-                "I have been tasked to guide you in constructing a suitable spacecraft for space tourism",
-                "Your first test is to build a spacecraft that is able to reach our planetary starport",
-                "Be careful though. You want to avoid the space trash layer orbitting the Earth",
-                "Navigate the rocket using the left and right thrusters to avoid the following rubbish obstacles...",
+            readString  = new string[14] {
+                "Yeah...um...it is currently not economically feasible to throw rubbish into the sun.", 
+                "Oh well. Can't say I didn't try", 
+                "Currently, Earth is not going to be habitable in the foreseeable future", 
+                "People will eventually be migrating to collosal starports orbitting each planet", 
+                "They want to experience what it is like to live on a starport before making their big commitment to move there", 
+                "And so, for this mission we need to deliver tourists to our planetary starports scattered across our solor system", 
+                "Make sure to add the amount of tourists you want to deliver to a starport. They ain't picky on which one they arrive at", 
+                "To add tourists, navigating to the tourist screen via the tab in the build screen. You can also upgrade the passenger bay size to hold more tourists", 
+                "The tourist screen looks like this...", 
                 "",
-                "Once you are close enough, the starport's tractor beam will guide you in for docking.\n\nThe starport looks like this...",
-                "",
-                "\u0022When something is important enough, you do it even if the odds are not in your favor\u0022",
-                "Good Luck!"
+                "Any successful tourist delivery will result in a money bonus for you to spend", 
+                "Please do not let the rocket explode with tourists onboard. Our profits will decrease as we pay for life damages", 
+                "This is going to be a long mission.You will need unrelenting focus to achieve this task. Stay strong!", 
+                "We're going to make it happen. As God is my bloody witness, I'm hell-bent on making it work."
             };
         }
         Debug.Log(readString[0]);
@@ -109,26 +108,39 @@ public class AnimatedDialog : MonoBehaviour {
             {
                 continue;
             }
-
-            if (stringIndex == 5)
+            if (stringIndex == 5 && levelNumber == 1)
             {
                 rubbishImages.SetActive(true);
             }
-
             else
             {
                 rubbishImages.SetActive(false);
             }
-
-            if (stringIndex == 7)
+            if (stringIndex == 7 && levelNumber == 1)
             {
                 starportImage.SetActive(true);
             }
-
             else
             {
                 starportImage.SetActive(false);
             }
+            if (stringIndex == 9 && levelNumber == 2)
+            {
+                ecoProductImage.SetActive(true);
+            }
+            else
+            {
+                ecoProductImage.SetActive(false);
+            }
+            if (stringIndex == 9 && levelNumber == 3)
+            {
+                touristImage.SetActive(true);
+            }
+            else
+            {
+                touristImage.SetActive(false);
+            }
+
 
             textArea.text = readString[stringIndex].Substring(0, characterIndex);
             characterIndex++;
